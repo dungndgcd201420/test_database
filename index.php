e71621fb46bf42d4a62c61a3c2e0d23b7126cc43
unknown <dungndgcd201420@fpt.edu.vn>
2022-02-23 08:23:45 +0700

    add

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html> 
+<html>
+<head>
+	<title>Lab Website</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<h1>Welcome Everyone</h1>
+</head>
+  <body>
+  	<div class ="view-container">
+  	<div><img src="view.jpg" alt="view"></div>
+  		<div><a href="login.php">Login</a></div>
+  	</div>
+  </body>
+</html>
+
+
